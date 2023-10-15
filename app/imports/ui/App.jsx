eb80927e2f12c916aad139bd026b9e4f690d179b
Kaianu Reyes-Huynh <kaianu@hawaii.edu>
2023-10-15 12:54:56 -1000

    Section 3

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '../api/TasksCollection';
 import { Task } from './Task.jsx';
+import { TaskForm } from './TaskForm';
 
 const tasks = [
   {_id: 1, text: 'First Task'},
@@ -10,11 +11,13 @@ const tasks = [
 ]
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm/>
 
       <ul>
         { tasks.map(task => <Task key={ task._id } task={ task }/>) }
